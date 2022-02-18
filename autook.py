ceaa370aa556e323ffc86baeaeb4bbfe2229876a
Angelmar42 <76743934+Angelmar42@users.noreply.github.com>
2022-02-18 12:18:05 -0600

    Add files via upload

---
+++ autook.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pyautogui
+import time
+
+time.sleep(10)
+pyautogui.moveTo(300, 100,2)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.press('down')
+time.sleep(1)
+pyautogui.press('up')
+time.sleep(3)
+pyautogui.press('enter')
+
